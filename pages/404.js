fc6748e893ca7ff50794c24143aa37d7dff9942d
stephen <54988149+sharoskyy@users.noreply.github.com>
2022-09-22 20:30:44 +0300

    Create 404.js

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+// 404.js
+import Link from 'next/link'
+
+export default function FourOhFour() {
+  return <>
+    <img src="https://quak.ovh/9cD8x2N.png" />
+    <Link href="/">
+      <a>
+        Go back home
+      </a>
+    </Link>
+  </>
+}
