792db822216bfc7b9799371bbfc1b564dd1601b2
stephen <54988149+sharoskyy@users.noreply.github.com>
2022-09-26 23:38:19 +0300

    new project

---
+++ pages/bhood.js
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../styles/Project.module.css'
+
+export default function Project() {
+    return(
+    <>
+    <Head>
+        <title>BROTHERHOOD Project</title>
+        <meta name="description" content="landing page for bhood.ro" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
+
+    <div className={styles.stefanMain}>
+        <div className={styles.goBack}>
+            <Link href="/">go back</Link>
+        </div>
+
+        <div className={styles.projectExpand}>
+            <div className={styles.projectTitle}>
+                <h1>BROTHERHOOD ROMÂNIA</h1>
+            </div>
+            <div className={styles.projectDescription}>
+                <p>landing page made for BROTHERHOOD ROMÂNIA community. made in <span class={styles.used}>html/css</span></p>
+                  <br />
+                <p>I do not own the fist logo.</p> 
+            </div>
+            <div className={styles.projectFull}>
+                <img src="../full/bhood.png" />
+            </div>
+        </div>
+    </div>
+    </>
+    )
+}
