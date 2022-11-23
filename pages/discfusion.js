c939e7e028c43b763bece8a4938059d3e280bbad
stephen <54988149+sharoskyy@users.noreply.github.com>
2022-11-23 13:20:03 +0200

    Update discfusion.js

--- pages/discfusion.js
+++ pages/discfusion.js
@@ -24,8 +24,8 @@ export default function Project() {
                 <p>landing page for discfusion, i don't own the logo rights but the owner allowed me to use it as thumbnail. made in <span class={styles.used}>html/css and js for animations</span></p> 
             </div>
             <div className={styles.projectFull}>
-                <img src="../full/virtfusionfull.png"/>
-                <img src="../full/virtfusionfullgif.gif"/>
+                <img src="../full/discfusion.png"/>
+                <img src="../full/discfusion.gif"/>
             </div>
         </div>
     </div>
